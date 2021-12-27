13de51ccac02f845181fb2ef4ce95aafad59d9d5
Paulo Eleison <paulo.Eleison@gmail.com>
2021-12-27 16:46:56 -0300

    Deracao

--- AprendendoGit/Program.cs
+++ AprendendoGit/Program.cs
@@ -6,12 +6,15 @@ namespace Curso
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("------------------------------");
             Console.WriteLine("Olá mundo");
 
             Console.WriteLine("Como você está");
 
             Console.WriteLine("Tenha um bom dia ");
+
+            Console.WriteLine("-------------------------------");
+
         }
 
     }
